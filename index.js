50d867c2084eec03c5c51cc94f77048a7b841f43
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-08-31 04:43:31 +0100

    typo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", PostRoutes);
-app.use("/api/v1/dalle", dalle);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Application is running");
